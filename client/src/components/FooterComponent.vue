c02093ceff100441f71842f1da23c195aa226cd8
KLag-SF <k.lagopus.sf19@gmail.com>
2021-06-12 02:54:25 +0900

    complete footer

--- client/src/components/FooterComponent.vue
+++ client/src/components/FooterComponent.vue
@@ -1,6 +1,15 @@
 <template>
-    <h1>Footer</h1>
+    <div class="footer">
+        <p class="copyright"> Copyright 2021 F0gr1, KLag-SF </p>
+    </div>
 </template>
+
+<style>
+    .copyright{
+        color: darkgrey;
+        text-align: center;
+    }
+</style>
 
 <script>
 export default {
