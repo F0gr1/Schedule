2f6c2605b8d9554b7ce6f3a6a23764d18bcfab95
KLag-SF <k.lagopus.sf19@gmail.com>
2021-06-17 13:58:10 +0900

    create HeaderComponent

--- client/src/components/HeaderComponent.vue
+++ client/src/components/HeaderComponent.vue
@@ -1,17 +1,52 @@
 <template>
-    <h1>Header</h1>
+  <div class="header">
+    <v-app-bar flat>
+
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      bottom
+      temporary
+    >
+      <v-list>
+        <v-list-item>
+
+          <v-list-item-title>
+            <router-link> Home </router-link>
+          </v-list-item-title>
+
+          <v-list-item-title>
+            <router-link> {{userName}} </router-link>
+          </v-list-item-title>
+
+          <v-list-item-title>
+            <router-link> Logout </router-link>
+          </v-list-item-title>
+
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Header',
-    data(){
-        return{
-            
-        }
-    },
-    methods:{
+  name: 'Header',
+  data(){
+      return{
+        userName: '',
+      };
+  },
 
+  methods:{
+    getUserName(){
+      
+    },
+
+    mounted(){
+      this.getUserName();
     }
+  }
 }
 </script>
